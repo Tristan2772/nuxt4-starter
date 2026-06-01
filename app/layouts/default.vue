87f3553c8135b0d7b886e5452a38365889fc883a
Tristan <tristancarter2772@gmail.com>
2026-05-31 20:45:06 -0500

    stripped out specifics for starter

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,10 +1,8 @@
 <script lang="ts" setup>
-import AppTitleNavbar from "../components/app/title-navbar.vue";
 </script>
 
 <template>
   <div class="flex flex-col min-h-screen">
-    <AppTitleNavbar />
     <main>
       <slot />
     </main>
